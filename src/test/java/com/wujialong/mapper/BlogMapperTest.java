b541703eb1a010e5a28a84caedd82cbb0a46e2f7
巫佳龙 <hello_turtle@163.com>
2016-12-02 19:29:51 +0800

    配置log4j输出到tomcat.out

--- src/test/java/com/wujialong/mapper/BlogMapperTest.java
+++ src/test/java/com/wujialong/mapper/BlogMapperTest.java
@@ -4,6 +4,7 @@ package com.wujialong.mapper;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +14,7 @@ import com.wujialong.pojo.Blog;
 
 public class BlogMapperTest {
 	ApplicationContext applicationContext;
+	Logger logger=Logger.getLogger(this.getClass());
 	@Before
 	public void setUp() throws Exception {
 		applicationContext=new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
@@ -24,8 +26,10 @@ public class BlogMapperTest {
 		HashMap<String, Comparable> map=new HashMap<String, Comparable>();
 		map.put("formerBlogID", 0);
 		map.put("type", "测试");
-		List<Blog> list=bm.getManageInformationByType(map);
-		System.out.println(list.get(0).getType());
+//		List<Blog> list=bm.getManageInformationByType(map);
+//		System.out.println(list.get(0).getType());
+		Blog blog=bm.getBlogByID(120);
+		logger.error("testManagerDate:"+blog.getReleaseDate());
 	}
 
 }
